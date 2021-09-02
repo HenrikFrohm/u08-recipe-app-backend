072e6f687bd8e52d1bb1e60d878d40a7ed980f5b
woxer <woxer123@gmail.com>
2021-09-02 11:27:15 +0200

    Procfile for Heroku. Added migration, M and C.

---
+++ app/Http/Controllers/RecipeListController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+
+class RecipeListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (auth()->user()) {
+            $recipe_lists = RecipeList::all()->toArray();
+            return $recipe_lists;
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => "No recipe lists found",
+            ]);
+        }
+    }
+
+    public function get($id)
+    {
+        if (auth()->user()) {
+            $recipe_list = RecipeList::find($id);
+            return $recipe_list;
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Recipe list do not exist'
+            ]);
+        }
+    }
+
+     public function store(Request $request)
+     {
+        $input = $request->all();
+        if(auth()->user()) {
+            $list = RecipeList::create(['title' => $request->input('title'), 'user_id' => auth()->user()->id]);
+            return response()->json([
+                'success' => true,
+                'recipeList' => $list
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'sorry, recipe list could not be added'
+            ]);
+        }
+     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RecipeList  $recipelist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if(auth()->user()) {
+            $recipeList = RecipeList::find($id);
+            $recipeList->update($request->all());
+            return $recipeList;
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Recipe list didnt get updated'
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Recipe  $recipe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(auth()->user()) {
+            $list = RecipeList::find($id)->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Recipe list was deleted',
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'The recipe list didnt get deleted'
+            ]);
+        }
+    }
+}
